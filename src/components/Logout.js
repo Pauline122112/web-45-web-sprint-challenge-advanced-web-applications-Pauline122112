a6fd817f92a77f954ed5ada7014fb158fbb3085b
Pauline122112 <pauline.design007@gmail.com>
2021-08-20 12:22:04 -0500

    added useEffect in logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,17 +1,18 @@
 import React, { useEffect } from "react";
+import axiosWithAuth from '../helpers/axiosWithAuth'
 
 
 const Logout = (props) => {
-	// useEffect(() => {
-	// 	axiosWithAuth()
-	// 		.post("/logout")
-	// 		.then((res) => {
-	// 			localStorage.removeItem("token");
-	// 			localStorage.removeItem("role", res.data.role);
-	// 			localStorage.removeItem("username", res.data.username);
-	// 			props.history.push("/login");
-	// 		});
-	// }, []);
+	useEffect(() => {
+		axiosWithAuth()
+			.post("/logout")
+			.then((res) => {
+				localStorage.removeItem("token");
+				localStorage.removeItem("role", res.data.role);
+				localStorage.removeItem("username", res.data.username);
+				props.history.push("/login");
+			});
+	}, [props]);
 
 	return <div></div>;
 };
