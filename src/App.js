fb77f78abbabb4ae372d5481fd4072aab85ea30b
Pauline122112 <pauline.design007@gmail.com>
2021-08-19 22:14:24 -0500

    added routes that link to login component in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -17,6 +17,11 @@ function App() {
 						logout
 					</a>
 				</header>
+
+				<Switch>
+					<Route path="/login" component={Login} />
+					<Route path="/" component={Login} />
+				</Switch>
 			</div>
 		</Router>
 	);
